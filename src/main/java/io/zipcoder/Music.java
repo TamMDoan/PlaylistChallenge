5e079c134faa23e1eed8ff13f938a967eaa14f56
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-09 18:08:37 -0500

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,29 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int countForwards = 0;
+        int countBackwards = 0;
+        for(int i = startIndex; i < this.playList.length; i++){
+            if(this.playList[i].equals(selection)){
+                break;
+            }
+            countForwards++;
+        }
+
+        for(int i = startIndex; i < this.playList.length; i--){
+            if(this.playList[i].equals(selection)){
+                break;
+            }
+            if(i == 0){
+                i = this.playList.length-1;
+            }
+            countBackwards++;
+        }
+
+        if(countForwards < countBackwards){
+            return countForwards;
+        }
+
+        return countBackwards;
     }
 }
